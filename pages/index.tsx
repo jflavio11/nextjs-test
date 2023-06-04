c5a31f013154a232b2ab25630c00e78c679d547c
Jose Flavio <jflavio90@gmail.com>
2023-06-03 21:35:37 -0500

    add comment about useEffect

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ const HomePage = () => {
 
   const [productList, setProductList] = useState<TProduct[]>([])
 
-  useEffect(() => {
+  useEffect(() => { // always executed in browser (client side rendered)
     window
     .fetch('api/avo')
     .then(response => response.json())
